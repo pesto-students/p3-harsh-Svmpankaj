57ddddb9b34ab09bd3ad71fb91b28163f1769276
Svmpankaj <svmpankaj@gmail.com>
2022-09-13 20:15:20 +0530

    format code

--- Week-8/8.6/index.js
+++ Week-8/8.6/index.js
@@ -2,24 +2,24 @@ function path(graph) {
     const results = [];
     findPaths([0], 0, graph, results);
     return results;
-  }
+}
 
-  function findPaths(current, node, graph, results) {
+function findPaths(current, node, graph, results) {
     if (node == graph.length - 1) {
         results.push(current.slice(0));
         return;
     }
-    
-    for(let i = 0; i < graph[node].length; i++) {
+
+    for (let i = 0; i < graph[node].length; i++) {
         current.push(graph[node][i]);
         findPaths(current, graph[node][i], graph, results);
         current.pop();
     }
-  }
+}
 
 //   path([[1,2]]);
 //   path([[3]]);
 //   path([[3]]);
 //   path([[]]);
-  
-  console.log(path([[1,2],[3],[3],[]]));
+
+console.log(path([[1, 2], [3], [3], []]));
